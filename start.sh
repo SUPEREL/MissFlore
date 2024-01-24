ebb1b1daae9f48d28eafe340b542f4c81f9c88a3
KPEDJO <108293740+SUPEREL@users.noreply.github.com>
2024-01-24 03:22:32 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
-
+then
   echo "Cloning main Repository"
-  git clone https://github.com/SUPEREL/MissFlore.git /MissFlore
+  git clone https://github.com/NobiDeveloper/Nobita-Filter-Bot.git /Nobita-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MissFlore
+  git clone $UPSTREAM_REPO /Nobita-Filter-Bot
 fi
-cd /MissFlore
+cd /Nobita-Filter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting...."
 python3 bot.py
